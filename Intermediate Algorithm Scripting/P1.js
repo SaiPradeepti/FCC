b5b5b1f3320f847c6b9ffbce89166fab498edded
SaiPradeepti <69248387+SaiPradeepti@users.noreply.github.com>
2020-08-14 22:13:31 +0530

    Sum All Numbers in a Range

--- Intermediate Algorithm Scripting/P1.js
+++ Intermediate Algorithm Scripting/P1.js
@@ -1,5 +1,6 @@
 // Problem - Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
 
+
 function sumAll(arr) {
     let sum = 0;
 
